244c45cb3a0055e90f1378abe9bc713f7c12dffc
Martin Van Elslande <martin.van-elslande@epitech.eu>
2018-02-25 07:12:40 +0100

    Both programs now check for elf version in e_ident

--- objdump/src/errors.c
+++ objdump/src/errors.c
@@ -13,7 +13,8 @@ int	check_magic_number(unsigned char ident[])
 	if (ident[EI_MAG0] == ELFMAG0 &&
 	ident[EI_MAG1] == ELFMAG1 &&
 	ident[EI_MAG2] == ELFMAG2 &&
-	ident[EI_MAG3] == ELFMAG3)
+	ident[EI_MAG3] == ELFMAG3 &&
+	ident[EI_VERSION] == 1)
 		return (1);
 	return (0);
 }
